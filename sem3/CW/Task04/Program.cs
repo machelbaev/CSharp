68455512f7c017d6172666f0789aa78909d0faeb
Михаил Челбаев <machelbaev@edu.hse.ru>
2019-04-26 00:00:44 +0300

    operators

---
+++ sem3/CW/Task04/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace Task04
+{
+    public class Atom
+    {
+        public Atom(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int Z { get; set; }
+
+        public int Attempts { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Atom atom &&
+                   X == atom.X &&
+                   Y == atom.Y &&
+                   Z == atom.Z;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -307843816;
+            hashCode = hashCode * -1521134295 + X.GetHashCode();
+            hashCode = hashCode * -1521134295 + Y.GetHashCode();
+            hashCode = hashCode * -1521134295 + Z.GetHashCode();
+            return hashCode;
+        }
+
+        public static bool operator ==(Atom a, Atom b)
+        {
+            if (a.X == b.X && a.Y == b.Y && a.Z == b.Z)
+                return true;
+            return false;
+        }
+
+        public static bool operator !=(Atom a, Atom b)
+        {
+            if (a.X == b.X && a.Y == b.Y && a.Z == b.Z)
+                return false;
+            return true;
+        }
+
+        public override string ToString()
+        {
+            return $"X = {X}, Y = {Y}, Z = {Z}, attempts = {Attempts}";
+        }
+    }
+
+    class Program
+    {
+        static Random rnd = new Random();
+
+        static int number = 1000;
+
+        static void Main(string[] args)
+        {
+            Atom[] atoms = new Atom[number];
+            for (int i = 0; i < atoms.Length; i++)
+            {
+                atoms[i] = new Atom(rnd.Next(11), rnd.Next(11), rnd.Next(11));
+            }
+            int count = 1;
+            while (count < number)
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    if (atoms[i] == atoms[count])
+                    {
+                        atoms[count].X = rnd.Next(10);
+                        atoms[count].Y = rnd.Next(10);
+                        atoms[count].Z = rnd.Next(10);
+                        atoms[count].Attempts++;
+                        count = 1;
+                        break;
+                    }
+                }
+                count++;
+            }
+
+            //display atoms
+            foreach (var item in atoms)
+                Console.WriteLine(item.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ sem3/CW/Task06/Program.cs
@@ -0,0 +1,143 @@
+﻿/*
+ Student: Chelbaev Mikhail
+ Group: BPI182_2
+ Task:
+*/
+
+using System;
+
+namespace Task06
+{
+    public class Polynomial2
+    {
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="a">coefficient</param>
+        /// <param name="b">coefficient</param>
+        /// <param name="c">coefficient</param>
+        public Polynomial2(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        /// <summary>
+        /// constructor which copy values from another class
+        /// </summary>
+        /// <param name="polinomial"></param>
+        public Polynomial2(Polynomial2 polinomial)
+        {
+            A = polinomial.A;
+            B = polinomial.B;
+            C = polinomial.C;
+        }
+
+        /// <summary>
+        /// coefficient a
+        /// </summary>
+        double a;
+
+        /// <summary>
+        /// property for coefficient a
+        /// </summary>
+        public double A
+        {
+            set
+            {
+                if (value == 0)
+                    throw new ArgumentException("Coefficient 'a' can't be 0!");
+                a = value;
+            }
+            get => a;
+        }
+
+        /// <summary>
+        /// coefficient b
+        /// </summary>
+        public double B { get; set; }
+
+        /// <summary>
+        /// coefficient c
+        /// </summary>
+        public double C { get; set; }
+
+        /// <summary>
+        /// Calculates the value in x
+        /// </summary>
+        /// <param name="x">number</param>
+        /// <returns>value in x</returns>
+        public double Value(double x)
+        {
+            return A * x * x + B * x + C;
+        }
+
+        public static Polynomial2 operator +(Polynomial2 a, Polynomial2 b)
+        {
+            return new Polynomial2(a.A + b.A, a.B + b.B, a.C + b.C);
+        }
+
+        public static Polynomial2 operator -(Polynomial2 a, Polynomial2 b)
+        {
+            return new Polynomial2(a.A - b.A, a.B - b.B, a.C - b.C);
+        }
+
+        public static Polynomial2 operator *(Polynomial2 a, double b)
+        {
+            return new Polynomial2(a.A * b, a.B * b, a.C * b);
+        }
+
+        public static Polynomial2 operator /(Polynomial2 a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException();
+            return new Polynomial2(a.A / b, a.B / b, a.C / b);
+        }
+
+        /// <summary>
+        /// override ToString() method
+        /// </summary>
+        /// <returns>polynomial</returns>
+        public override string ToString()
+        {
+            return $"{A:f1}*x^2+{B:f1}*x+{C:f1}";
+        }
+    }
+
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Clear();
+
+                try
+                {
+                    Polynomial2 p1 = new Polynomial2(-2, 3, 5);
+                    Polynomial2 p2 = new Polynomial2(p1);
+                    p2.B = -2;
+                    p2.C = 4;
+                    Console.WriteLine($"Polynomial {p1} / 3 = {p1 / 3}");
+                    Console.WriteLine($"Polynomial {p1} * 2 = {p1 * 2}");
+                    Console.WriteLine($"Polynomial {p1} + polynomial {p2} = {p1 + p2}");
+                    Console.WriteLine($"Polynomial {p1} - polynomial {p2} = {p1 - p2}");
+                }
+                catch (DivideByZeroException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+                Console.WriteLine("Enter Esc to exit...");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
+
+
